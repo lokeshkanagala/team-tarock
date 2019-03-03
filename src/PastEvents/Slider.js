cf45ce87b6aeac402d8a8251eaa8cc68e68a2958
lokeshkanagala <lokesh.kanagala@gmail.com>
2019-03-03 17:18:28 -0500

    add style" "

--- src/PastEvents/Slider.js
+++ src/PastEvents/Slider.js
@@ -11,10 +11,10 @@ export default class Slider extends React.Component {
       const params = {
       effect: 'coverflow',
       grabCursor: true,
-      slidesPerView: 4,
+      slidesPerView: window.innerWidth < 600? 1:4,
       coverflowEffect: {
         rotate: 50,
-        stretch: 0,
+        stretch: 10,
         depth: 100,
         modifier: 1,
         slideShadows: true
@@ -32,18 +32,18 @@ export default class Slider extends React.Component {
     return(
       <div height="50px !important">
         <Swiper {...params}>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetupfirst.png"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup1.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup2.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup3.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup4.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup5.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup6.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup7.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup8.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup9.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup10.jpeg"}/></div>
-          <div><img style={{height:"300px"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup11.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetupfirst.png"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup1.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup2.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup3.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup4.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup5.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup6.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup7.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup8.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup9.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup10.jpeg"}/></div>
+          <div><img style={{height:"300px", width:"100%"}} src={"https://s3.ap-south-1.amazonaws.com/www.teamtaraktrust.com/store/meetup11.jpeg"}/></div>
         </Swiper>
       </div>
     )
